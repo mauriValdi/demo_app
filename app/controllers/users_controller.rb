c9b17b63d739f3acabe78ccbeb2a29fd5fb02c3b
mauriValdi <mauricioo.valdi@hotmail.com>
2017-04-10 22:45:59 -0400

    finished toy app for deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @micropost = Micropost.find_by user_id: (params[:id])
+    #@micropost = Micropost.find_by user_id: (params[:id])
   end
 
 
